8682634b4e5e3cf1711f4a917a0f0fb66855341d
Piyush293 <41056102+Piyush293@users.noreply.github.com>
2023-07-06 14:42:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 import matplotlib.pyplot as plt
 import yfinance as yf
 from keras.models import load_model
-import streamlit as st
 from st_on_hover_tabs import on_hover_tabs
 from PIL import Image
 import setup
